a73e60a37ccbd9fa7b9adab3efd9fec9b26e5691
Vitor Hugo <vitor4oliveira@gmail.com>
2022-02-07 00:15:38 -0300

    fix: text of main component

--- src/components/template/Main.jsx
+++ src/components/template/Main.jsx
@@ -5,7 +5,7 @@ import PokemonData from '../../data/PokemonData'
 const mainComponent = props =>
     <main className="main">
         <h1>Welcome to Pokedéx with React</h1>
-        <p>Type a pokemon name or id to start</p>
+        <p>Type a pokemon name to start</p>
         <PokemonData />
     </main>
 export default mainComponent
